0f8f55c4105a674f731965737a7b8d9e47c9e6f3
Dharmraj Vaghela <vagheladharmraj06@gmail.com>
2025-03-10 23:32:30 +0530

    added addtogrid function

--- logic.cpp
+++ logic.cpp
@@ -10,9 +10,6 @@ const int HEIGHT = 20;
 
 using namespace std;
 
-// merge tetromino
-// can the shape of the tetromino be placed or not
-
 // rotate the tetromino
 enum tetro
 {
@@ -29,27 +26,29 @@ vector<vector<bool>> get_tetromino(tetro curr_tetro);
 
 vector<vector<bool>> rotate(tetro curr_tetro, int rnum)
 {
-    
-    vector<vector<bool>> shape = get_tetromino(curr_tetro); // Tetrominos will be accessed from the file itself and not the vector
-    if(rnum%4 == 0)return shape;
 
-    for (int r = 0; r < rnum%4; ++r)
+    vector<vector<bool>> shape = get_tetromino(curr_tetro); // Tetrominos will be accessed from the file itself and not the vector
+    if (rnum % 4 == 0)
+        return shape;
+
+    for (int r = 0; r < rnum % 4; ++r)
     {
-        vector<vector<bool>>new_shape(shape[0].size(), vector<bool>(shape.size()));
-        for (size_t i = 0; i < shape.size(); ++i)
+        vector<vector<bool>> new_shape(shape[0].size(), vector<bool>(shape.size()));
+        for (int i = 0; i < shape.size(); ++i)
         {
-            for (size_t j = 0; j < shape[0].size(); ++j)
+            for (int j = 0; j < shape[0].size(); ++j)
             {
-            new_shape[j][shape.size() - 1 - i] = shape[i][j];
+                new_shape[j][shape.size() - 1 - i] = shape[i][j];
             }
         }
-        shape =new_shape;
+        shape = new_shape;
     }
 
     return shape;
 }
 
-int clear_lines(vector<vector<int>> grid)
+//checks and clears the lines which are full
+int clear_lines(vector<vector<bool>> &grid)
 {
     int score = 0;
     for (int y = HEIGHT - 1; y >= 0; y--)
@@ -70,7 +69,7 @@ int clear_lines(vector<vector<int>> grid)
             {
                 grid[yy] = grid[yy - 1];
             }
-            grid[0] = vector<int>(WIDTH, 0);
+            grid[0] = vector<bool>(WIDTH, 0);
             y++;
         }
     }
@@ -78,4 +77,20 @@ int clear_lines(vector<vector<int>> grid)
     return score;
 }
 
+int CurrX, CurrY;
+//when the tetromino is placed
+void addtogrid(vector<vector<bool>> &grid, tetro curr_tetro)
+{
 
+    vector<vector<bool>> shape = get_tetromino(curr_tetro);
+    for (int i = 0; i < shape.size(); i++)
+    {
+        for (int j = 0; j < shape[0].size(); j++)
+        {
+            if (shape[i][j])
+            {
+                grid[CurrX + i][CurrY + j] = 1;
+            }
+        }
+    }
+}
