7852921d3425cf802452fa0364fa1fe40db9493b
Dharmraj Vaghela <vagheladharmraj06@gmail.com>
2025-03-13 17:09:41 +0530

    created class for logic

--- logic.cpp
+++ logic.cpp
@@ -1,26 +1,24 @@
-#include <bits/stdc++.h>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <conio.h>
 #include <windows.h>
 #include "logic.h"
+#include "tetriminos.h"
 
+Logic::Logic() : CurrX(0), CurrY(0) {}
 
-vector<vector<bool>> rotate(tetro curr_tetro, int rnum)
-{
-
-    vector<vector<bool>> shape = get_tetromino(curr_tetro); // Tetrominos will be accessed from the file itself and not the vector
+vector<vector<bool>> Logic::rotate (tetro curr_tetro, int rnum) {
+    vector<vector<bool>> shape = get_tetromino(curr_tetro);
     if (rnum % 4 == 0)
         return shape;
 
-    for (int r = 0; r < rnum % 4; ++r)
-    {
+    for (int r = 0; r < rnum % 4; ++r) {
+
         vector<vector<bool>> new_shape(shape[0].size(), vector<bool>(shape.size()));
-        for (int i = 0; i < shape.size(); ++i)
-        {
-            for (int j = 0; j < shape[0].size(); ++j)
-            {
+        for (int i = 0; i < shape.size(); ++i) {
+
+            for (int j = 0; j < shape[0].size(); ++j) {
                 new_shape[j][shape.size() - 1 - i] = shape[i][j];
             }
         }
@@ -30,27 +28,25 @@ vector<vector<bool>> rotate(tetro curr_tetro, int rnum)
     return shape;
 }
 
-int clear_lines(vector<vector<bool>> &grid)
-{
+int Logic::clear_lines(vector<vector<bool>> &grid) {
     int score = 0;
-    for (int y = HEIGHT - 1; y >= 0; y--)
-    {
+
+    for (int y = HEIGHT - 1; y >= 0; y--) {
         bool full = true;
-        for (int x = 0; x < WIDTH; x--)
-        {
-            if (!grid[y][x])
-            {
+        
+        for (int x = 0; x < WIDTH; x--) {
+            if (!grid[y][x]) {
                 full = false;
                 break;
             }
         }
-        if (full)
-        {
+        if (full) {
             score++;
-            for (int yy = y; yy > 0; yy--)
-            {
+           
+            for (int yy = y; yy > 0; yy--) {
                 grid[yy] = grid[yy - 1];
             }
+           
             grid[0] = vector<bool>(WIDTH, 0);
             y++;
         }
@@ -59,19 +55,31 @@ int clear_lines(vector<vector<bool>> &grid)
     return score;
 }
 
-int CurrX, CurrY;
-void addtogrid(vector<vector<bool>> &grid, tetro curr_tetro)
-{
+void Logic::addtogrid(vector<vector<bool>> &grid, tetro curr_tetro) {
 
     vector<vector<bool>> shape = get_tetromino(curr_tetro);
-    for (int i = 0; i < shape.size(); i++)
-    {
-        for (int j = 0; j < shape[0].size(); j++)
-        {
-            if (shape[i][j])
-            {
+
+    for (int i = 0; i < shape.size(); i++) {
+        for (int j = 0; j < shape[0].size(); j++) {
+            if (shape[i][j]) {
                 grid[CurrX + i][CurrY + j] = 1;
             }
         }
     }
+}
+
+bool Logic::canPlace(int x, int y, const vector<vector<bool>>& shape, const vector<vector<int>>& field) {
+    
+    for (int i = 0; i < shape.size(); ++i) {
+        for (int j = 0; j < shape[0].size(); ++j) {
+            if (shape[i][j]) {
+                
+                if (x + j < 0 || x + j >= field[0].size() || y + i >= field.size() || (y + i >= 0 && field[y + i][x + j])) {
+                    return false;
+                }
+            }
+        }
+    }
+    
+    return true;
 }
--- logic.h
+++ logic.h
@@ -2,26 +2,24 @@
 #define LOGIC_H
 
 #include <vector>
+//#include "tetriminos.h"
 using namespace std;
 
 const int WIDTH = 10;
 const int HEIGHT = 20;
 
-enum tetro { L, O, I, S, Z, J, T };
+enum tetro { I, O, S, T, Z, L, J };
 
+class Logic {
+public:
+    Logic();
+    vector<vector<bool>> rotate(tetro curr_tetro, int rnum);
+    int clear_lines(vector<vector<bool>> &grid);
+    void addtogrid(vector<vector<bool>> &grid, tetro curr_tetro);
+    bool canPlace(int x, int y, const vector<vector<bool>>& shape, const vector<vector<int>>& field);
 
-
-
-vector<vector<bool>> get_tetromino(tetro curr_tetro);
-
-vector<vector<bool>> rotate(tetro curr_tetro, int rnum);
-
-int clear_lines(vector<vector<bool>> &grid);
-
-void addtogrid(vector<vector<bool>> &grid, tetro curr_tetro);
-
-
-
-int CurrX, CurrY;
+private:
+    int CurrX, CurrY;
+};
 
 #endif
