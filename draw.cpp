435b0f95f2af633118a10afd3ae138512ccb7a34
KNANY68 <9f52yuvansh.kesh@gmail.com>
2025-03-13 16:09:09 +0530

    logic included

--- draw.cpp
+++ draw.cpp
@@ -4,19 +4,17 @@
 #include <ctime>
 #include <conio.h> 
 #include <windows.h>
-#include <iostream>
-
 #include <logic.h>
 
 using namespace std;
+using namespace logic;
 
 int Width = 10;
 int Height = 20;
-int curr_tetro;
+int currentTetromino;
 int rnum;
-int CurrX;
-int CurrY;
-vector<vector<bool>> grid(Height, vector<bool>(Width, false));
+int currentX;
+int currentY;
 
 void drawGrid()
 {
@@ -37,38 +35,7 @@ void drawGrid()
     }
 }
 
-
-void control()
-{
-    if (_kbhit())
-        {
-            char key = _getch();
-            if (key == char(75) && canplace(CurrX
-             - 1, CurrY, shape))
-            {
-                CurrX
-            --;
-            }
-            else if (key == char(77) && canplace(CurrX
-             + 1, CurrY, shape))
-            {
-                CurrX
-            ++;
-            }
-            else if (key == char(72))
-            {
-                rnum = (rnum + 1) % 4;
-                if (!canplace(CurrX
-                , CurrY, get_tetromino(curr_tetro)))
-                {
-                    rnum = (rnum - 1 + 4) % 4; // Revert rotation if invalid
-                }
-            }
-            
-        }
-}
-
-bool canplace(int x, int y, const vector<vector<bool>>& shape, const vector<vector<int>>& field) {
+bool canPlace(int x, int y, const vector<vector<bool>>& shape, const vector<vector<int>>& field) {
     for (int i = 0; i < shape.size(); ++i) {
         for (int j = 0; j < shape[0].size(); ++j) {
             if (shape[i][j]) {
@@ -80,4 +47,42 @@ bool canplace(int x, int y, const vector<vector<bool>>& shape, const vector<vect
     }
     return true;
 }
-// if the return value of the function canplace is true then call addtogrid
+
+
+void control()
+{
+    if (_kbhit())
+        {
+            char key = _getch();
+            if (key == char(75) && canPlace(currentX - 1, currentY, shape))
+            {
+                currentX--;
+            }
+            else if (key == char(77) && canPlace(currentX + 1, currentY, shape))
+            {
+                currentX++;
+            }
+            else if (key == char(72))
+            {
+                rnum = (rnum + 1) % 4;
+                if (!canPlace(currentX, currentY, get_tetromino(curr_tetro)))
+                {
+                    rnum = (rnum - 1 + 4) % 4; // Revert rotation if invalid
+                }
+            }
+            
+        }
+}
+
+bool canPlace(int x, int y, const vector<vector<bool>>& shape, const vector<vector<int>>& field) {
+    for (int i = 0; i < shape.size(); ++i) {
+        for (int j = 0; j < shape[0].size(); ++j) {
+            if (shape[i][j]) {
+                if (x + j < 0 || x + j >= field[0].size() || y + i >= field.size() || (y + i >= 0 && field[y + i][x + j])) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
