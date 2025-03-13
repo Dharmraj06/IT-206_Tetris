670c934d0ff7a7c871164ab3fd7972a63c420c22
Dhvanit Vaghela <dhvanit525@gmail.com>
2025-03-13 16:40:46 +0530

    Add files via upload

--- tetriminos.cpp
+++ tetriminos.cpp
@@ -20,3 +20,46 @@ vector<vector<string>> tetrominoes = {
     {".X...X...XX....."},    //L
     {"..X...X..XX....."}     //J
 };
+
+
+class Tetromino {
+    public:
+        
+    };
+
+    class I : public Tetromino {
+    public:
+        I() : Tetromino({{1, 1, 1, 1}}) {}
+    };
+    
+    class J : public Tetromino {
+    public:
+        J() : Tetromino({{1, 0, 0}, {1, 1, 1}}) {}
+    };
+    
+    class O : public Tetromino {
+    public:
+        O() : Tetromino({{1, 1}, {1, 1}}) {}
+    };
+    
+    class L : public Tetromino {
+    public:
+        L() : Tetromino({{0, 0, 1}, {1, 1, 1}}) {}
+    };
+    
+    class S : public Tetromino {
+    public:
+        S() : Tetromino({{0, 1, 1}, {1, 1, 0}}) {}
+    };
+    
+    class Z : public Tetromino {
+    public:
+        Z() : Tetromino({{1, 1, 0}, {0, 1, 1}}) {}
+    };
+    
+    class T : public Tetromino {
+    public:
+        T() : Tetromino({{0, 1, 0}, {1, 1, 1}}) {}
+    };
+
+    Tetromino *currentPiece = new Tetromino(rand() % 7);
