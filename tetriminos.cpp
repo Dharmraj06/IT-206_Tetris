04113c4bf480c0fef52c5186a102c7f07e8d30f7
Dharmraj Vaghela <vagheladharmraj06@gmail.com>
2025-03-15 22:14:30 +0530

    final

--- tetriminos.cpp
+++ tetriminos.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include <ctime>
 #include "tetriminos.h"
+#include "logic.h"
 #include <cstdlib>
 
 using namespace std;
@@ -15,15 +16,15 @@ char board[height][width];
 
 int currtetro = 0;
 
-vector<vector<bool>> Tetromino::get_tetromino(tetro curr_tetro) {
+vector<vector<bool>> get_tetromino(tetro curr_tetro) {
 
     return TETROMINOS[curr_tetro];
 }
 
-vector<vector<bool>> Tetromino::getRandomTetromino() {
+vector<vector<bool>> getRandomTetromino() {
 
     srand(static_cast<unsigned int>(time(nullptr)));
-    
+
     int index = rand() % TETROMINOS.size();
     return TETROMINOS[index];
 }
--- tetriminos.h
+++ tetriminos.h
@@ -4,30 +4,34 @@
 #include <vector>
 using namespace std;
 
-enum tetro { I,J,L,O,S,T,Z };
-
-class Tetromino {
-public:
-    vector<vector<bool>> get_tetromino(tetro curr_tetro);
-    vector<vector<bool>> getRandomTetromino();
-
-private:
-    const vector<vector<vector<bool>>> TETROMINOS = {
-        // I
-        {{1, 1, 1, 1}},
-        // J
-        {{1, 0, 0}, {1, 1, 1}},
-        // L
-        {{0, 0, 1}, {1, 1, 1}},
-        // O
-        {{1, 1}, {1, 1}},
-        // S
-        {{0, 1, 1}, {1, 1, 0}},
-        // T
-        {{0, 1, 0}, {1, 1, 1}},
-        // Z
-        {{1, 1, 0}, {0, 1, 1}}
-    };
+enum tetro
+{
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
 };
+
+vector<vector<bool>> get_tetromino(tetro curr_tetro);
+vector<vector<bool>> getRandomTetromino();
+
+const vector<vector<vector<bool>>> TETROMINOS = {
+    // I
+    {{1, 1, 1, 1}},
+    // J
+    {{1, 0, 0}, {1, 1, 1}},
+    // L
+    {{0, 0, 1}, {1, 1, 1}},
+    // O
+    {{1, 1}, {1, 1}},
+    // S
+    {{0, 1, 1}, {1, 1, 0}},
+    // T
+    {{0, 1, 0}, {1, 1, 1}},
+    // Z
+    {{1, 1, 0}, {0, 1, 1}}};
 
 #endif
