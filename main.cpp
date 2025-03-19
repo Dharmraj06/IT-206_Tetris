743c7fd198d38419464c71ec8232142b0cf7c700
KNANY68 <9f52yuvansh.kesh@gmail.com>
2025-03-19 22:39:55 +0530

    main

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include "draw.h"
+#include "tetriminos.h"
+#include "logic.h"
+
+using namespace std;
+
+void drawField(const vector<vector<bool>> &field)
+{
+
+    for (int y = 0; y < HEIGHT; ++y)
+    {
+
+        for (int x = 0; x < WIDTH; ++x)
+        {
+
+            if (field[y][x] == 0)
+                cout << ".";
+            else
+                cout << "#";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    srand(time(0));
+    vector<vector<bool>> field(HEIGHT, vector<bool>(WIDTH, 0));
+    Logic logic;
+
+    tetro currenttetro = static_cast<tetro>(rand() % 7);
+
+    vector<vector<bool>> shape = get_tetromino(currenttetro);
+    
+    int currentX = WIDTH / 2 - 2;
+    int currentY = 0;
+    bool gameOver = false;
+
+    while (true)
+    {
+        vector<vector<bool>> tempField = field;
+        system("cls");
+
+        for (int i = 0; i < shape.size(); ++i)
+        {
+            for (int j = 0; j < shape[0].size(); ++j)
+            {
+                if (shape[i][j] && currentY + i >= 0)
+                {
+                    tempField[currentY + i][currentX + j] = 1;
+                }
+            }
+        }
+
+        drawField(tempField);
+
+        cout << "tetromino: " << currenttetro << endl;
+        cout<<currentX<<" "<<currentY<<endl;
+
+        control(shape, currentX, currentY, tempField, gameOver);
+        
+        cout<<currentX<<" "<<currentY<<endl;
+
+        if (gameOver)
+        {
+            cout << "Game Over!" << endl;
+            break;
+        }
+
+        if (canPlace(currentX, currentY + 1, shape, field))
+        {
+            currentY++;
+        }
+        else
+        {
+            logic.addtogrid(field, currenttetro, currentX, currentY);
+            logic.clear_lines(field);
+
+
+            currenttetro = static_cast<tetro>(rand() % TETROMINOS.size());
+            shape = get_tetromino(currenttetro);
+            currentX = WIDTH / 2 - 2;
+            currentY = 0;
+
+            if (!canPlace(currentX, currentY, get_tetromino(currenttetro), field))
+            {
+                cout << "Game Over!" << endl;
+                break;
+            }
+        }
+
+    #ifdef _WIN32
+        Sleep(200);
+    #else
+        usleep(200000); // 200 milliseconds
+    #endif
+        system("cls"); 
+    }
+
+    return 0;
+}
